729e35a46aba2bbc332109bb0770d4e4b4937514
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-04-04 02:02:06 -0300

    feat: add rota para deletar um Pet pelo Id

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require('../models/Pet')
 // helpers
 const getToken = require('../helpers/get-token')
 const getUserByToken = require('../helpers/get-user-by-token')
+const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = class PetController {
 
@@ -69,5 +70,76 @@ module.exports = class PetController {
         const pets = await Pet.find().sort('-createdAt')
 
         res.status(200).json({ pets })
+    }
+
+    static async getAllUserPets(req, res) {
+
+        // pega usuario pelo token
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({ 'user._id': user._id }).sort('-createdAt')
+
+        res.status(200).json({ pets })
+    }
+
+    static async getAllUserAdoptions(req, res) {
+
+        // pega usuario pelo token
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({ 'adopter._id': user._id }).sort('-createdAt')
+
+        res.status(200).json({ pets })
+    }
+
+    static async getPetById(req, res) {
+        const id = req.params.id
+
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: "Id inválido!" })
+            return
+        }
+
+        const pet = await Pet.findOne({ _id: id })
+
+        if (!pet) {
+            res.status(404).json({ message: "Pet não encontrado!" })
+            return
+        }
+        res.status(200).json({ pet })
+    }
+
+    static async removePetById(req, res) {
+        const id = req.params.id
+
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: "Id inválido!" })
+            return
+        }
+
+        const pet = await Pet.findOne({ _id: id })
+
+        if (!pet) {
+            res.status(404).json({ message: "Pet não encontrado!" })
+            return
+        }
+
+        // verifica se o usuario logado registrou o pet que ta tentando deletar
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        console.log(pet.user._id.toString())
+        console.log(user._id.toString())
+
+        if(pet.user._id.toString() !== user._id.toString()){
+            res.status(422).json({ message: "Houve um problema ao processar a sua solicitação!" })
+            return
+        }
+
+        await Pet.findByIdAndRemove(id)
+        res.status(200).json({ message: "Pet removido com sucesso!" })
+
     }
 }
--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -7,5 +7,9 @@ const { imageUpload } = require('../helpers/image-upload')
 
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create)
 router.get('/', PetController.getAll)
+router.get('/mypets', verifyToken, PetController.getAllUserPets)
+router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
+router.get('/:id', PetController.getPetById)
+router.delete('/:id', verifyToken, PetController.removePetById)
 
 module.exports = router
