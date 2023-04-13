712f61db66620307c9ea1da8b5323f530401cbd9
João Paulo Santos <83318297+dev-joaopaulosantos@users.noreply.github.com>
2023-04-13 18:19:28 -0300

    fix: removida msg de erro ao cadastrar pet

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -60,7 +60,7 @@ module.exports = class PetController {
 
         try {
             const newPet = await pet.save()
-            res.status(422).json({ message: "Pet cadastrado com sucesso!", newPet })
+            res.status(201).json({ message: "Pet cadastrado com sucesso!", newPet })
         } catch (error) {
             res.status(500).json({ message: error })
         }
